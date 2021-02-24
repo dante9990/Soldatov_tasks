5d5a89228334aa72c9183b1c7cc564e3a5c5d249
dante9990 <56958306+dante9990@users.noreply.github.com>
2021-02-24 14:07:27 +0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 //compile scss into css
 function style() {
-    return gulp.src('app/scss/*.scss')
+    return gulp.src('app/scss/**/*.scss')
     .pipe(sass().on('error',sass.logError))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.stream());
@@ -17,7 +17,7 @@ function watch() {
            index: "/index.html"
         }
     });
-    gulp.watch('app/scss/*.scss', style)
+    gulp.watch('app/scss/**/*.scss', style)
     gulp.watch('./.html').on('change',browserSync.reload);
     gulp.watch('./js/*/.js').on('change', browserSync.reload);
 }
