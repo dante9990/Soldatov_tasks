d55a099c196e405b6a9f62416c515e917b611e04
dante9990 <56958306+dante9990@users.noreply.github.com>
2021-02-24 12:56:28 +0400

    переделал gulpfile поправил адаптивную верстку

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,57 @@
 'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
- 
-gulp.task('sass', function(done) {
-  gulp.src("app/scss/*.scss")
-      .pipe(sass())
-      .pipe(gulp.dest("app/css"))
-      .pipe(browserSync.stream());
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+//compile scss into css
+function style() {
+    return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+}
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./app",
+           index: "/index.html"
+        }
+    });
+    gulp.watch('app/scss/*.scss', style)
+    gulp.watch('./.html').on('change',browserSync.reload);
+    gulp.watch('./js/*/.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
+
+// var gulp = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var sass = require('gulp-sass');
+
+// gulp.task('sass', function(done) {
+//     gulp.src("scr/scss/*.scss")
+//         .pipe(sass())
+//         .pipe(gulp.dest("scr/css"))
+//         .pipe(browserSync.stream());
 
 
-  done();
-});
+//     done();
+// });
 
-gulp.task('serve', function(done) {
+// gulp.task('serve', function(done) {
 
-  browserSync.init({
-      server: "app/"
-  });
+//     browserSync.init({
+//         server: "src/"
+//     });
 
-  gulp.watch("app/sass/*.sass", gulp.series('sass'));
-  gulp.watch("app/*.html").on('change', () => {
-    browserSync.reload();
-    done();
-  });
+//     gulp.watch("scr/sass/*.sass", gulp.series('sass'));
+//     gulp.watch("scr/*.html").on('change', () => {
+//       browserSync.reload();
+//       done();
+//     });
+  
 
+//     done();
+// });
 
-  done();
-});
-
-gulp.task('default', gulp.series('sass', 'serve'));
+// gulp.task('default', gulp.series('sass', 'serve'));
