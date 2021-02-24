26f7295cc1ee6bdbf6154d1993de7381b7450395
dante9990 <56958306+dante9990@users.noreply.github.com>
2021-02-24 22:34:38 +0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
-//compile scss into css
+
 function style() {
     return gulp.src('app/scss/**/*.scss')
     .pipe(sass().on('error',sass.logError))
